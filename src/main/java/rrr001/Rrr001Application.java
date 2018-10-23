da70abfbc8047413a7fc725f9115df63c5f20833
Matt Cadorette <mcadorette@gmail.com>
2018-10-23 10:05:39 -0400

    Initial commit from Atomist

---
+++ src/main/java/rrr001/Rrr001Application.java
@@ -0,0 +1,12 @@
+package rrr001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rrr001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rrr001Application.class, args);
+	}
+}
